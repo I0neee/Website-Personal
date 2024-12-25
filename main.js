7ec0d8910fe1bc53a2c0b2f6000f1dafbacc29d2
I0neee <gum2376@gmail.com>
2024-12-25 14:39:44 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#menu").onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+const Somplak = document.querySelector(".content");
+document.querySelector("#cta").onclick = () => {
+    alert("Tekan humberger bar yang di atas(Garis 3)")
+};
