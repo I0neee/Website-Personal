1a828cd3de396c79e64ea0d5e6b923ba8264935e
I0neee <gum2376@gmail.com>
2024-12-25 14:30:29 +0700

    Delete Belajar Js directory

--- Belajar Js/main.js
+++
@@ -1,9 +0,0 @@
-const navbarNav = document.querySelector(".navbar-nav");
-document.querySelector("#menu").onclick = () => {
-    navbarNav.classList.toggle('active');
-};
-
-const Somplak = document.querySelector(".content");
-document.querySelector("#cta").onclick = () => {
-    alert("Tekan humberger bar yang di atas(Garis 3)")
-};
